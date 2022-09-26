2249deb3a49cedd97fa1b179acdcbdbacdc01319
student1 <dima.romakhov@gmail.com>
2022-09-26 19:30:55 +0300

    working script creating cards, need some fixes

---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+import {data} from "./hp.js";
+
+const container = document.querySelector(".container__flex");
+
+function createCard(data) {
+    const card = document.createElement("article");
+    card.className = "characters__card";
+
+    const img = document.createElement("img");
+    img.setAttribute("src", data.image);
+    img.setAttribute("width", 334);
+    img.setAttribute("height", 192);
+
+    const text = document.createElement("div");
+    text.className = "card__text";
+
+    const title = document.createElement("h2");
+    title.className = "card__title";
+    title.textContent = data.name;
+
+    const description = document.createElement("div");
+    description.className = "card__description";
+
+    const p1 = document.createElement("p");
+    p1.textContent = `Actor: ${data.actor}`;
+
+    const p2 = document.createElement("p");
+    p2.textContent = `Gender: ${data.gender}`;
+
+    const p3 = document.createElement("p");
+    p3.textContent = `House: ${data.house}`;
+
+    const p4 = document.createElement("p");
+    p4.textContent = `Wand core: ${data.wand}`;
+
+    const p5 = document.createElement("p");
+    p5.textContent = `Alive: ${true ? "yes" : "no"}`;
+
+    description.append(p1);
+    description.append(p2);
+    description.append(p3);
+    description.append(p4);
+    description.append(p5);
+
+    text.append(description);
+    text.append(title);
+
+    card.append(text);
+    card.append(img);
+
+    return card;
+}
+
+data.forEach(elem => {
+    container.append(createCard(elem))
+})
+
+
